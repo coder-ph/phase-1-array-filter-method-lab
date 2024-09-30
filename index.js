5413b9243583ed8b73973fc8a505990ed6cbee3c
Phelix <phelixmbani@gmail.com>
2024-09-30 18:02:31 +0300

    array filters

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution here
+// // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (collection, str){
+   const drivermatch = []
+    for(const driver of collection){
+        if ( driver.toLowerCase() === str.toLowerCase()){
+            
+            (drivermatch.push(driver))
+        }    
+    } return drivermatch
+}
+console.log(findMatching(drivers, "bobby"))
+
+function fuzzyMatch(collection, char) {
+    const drivers = [];
+    for (const driver of collection) {
+        // Compare only the first part of the driver's name with the 'char'
+        if (driver.slice(0, char.length) === char) {
+            drivers.push(driver);
+        }
+    }
+    return drivers;
+}
+
+const driversList = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+console.log(fuzzyMatch(driversList, "Sa")); 
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+function matchName(driverss, str){
+    const newDrivers = [];
+        for(const driver of driverss){
+            if(driver.name === str){
+                newDrivers.push(driver)
+            }
+        }return newDrivers
+}
+console.log(matchName(drivers, "Bobby"))
